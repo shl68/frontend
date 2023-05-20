4ce11a4b0410244cef638e2006c5485b3a1da7fe
shl68 <ahpen@naver.co>
2023-05-20 18:30:22 +0900

    1

---
+++ src/components/Card.vue
@@ -0,0 +1,34 @@
+<template>
+
+  <div class="card shadow-sm">
+    <img :src="item.imgPath" />
+
+    <div class="card-body">
+      <p class="card-text">{{ item.name }}</p>
+      <div class="d-flex justify-content-between align-items-center">
+      <button class="btn btn primary">구입하기</button>
+        <small class="text-muted">{{item.price}}원</small>
+      </div>
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+
+export default {
+  name: "Card",
+  props:{
+    item: Object
+  }
+
+
+}
+
+</script>
+
+<style scoped>
+
+
+</style>
